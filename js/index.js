943d294e6fb4a80fdddd08f5f31d0165332bb403
Alyssa <alyssanterry468@gmail.com>
2021-05-22 10:48:31 -0400

    Office Hours Help
    burger menu fixes etc

--- js/index.js
+++ js/index.js
@@ -1,60 +1,65 @@
 // Underline Navigation After Click
-$(document).ready(function() {
+$(document).ready(function () {
+  // Get current page URL
+  var url = window.location.href;
 
-    // Get current page URL
-    var url = window.location.href;
-   
-    // remove # from URL
-    url = url.substring(0, (url.indexOf("#") == -1) ? url.length : url.indexOf("#"));
-   
-    // remove parameters from URL
-    url = url.substring(0, (url.indexOf("?") == -1) ? url.length : url.indexOf("?"));
-   
-    // select file name
-    url = url.substr(url.lastIndexOf("/") + 1);
-    
-    // If file name not avilable
-    if(url == ''){
-    url = 'index.html';
-    }
-    
-    // Loop all menu items
-    $('.nav li').each(function(){
-   
-     // select href
-     var href = $(this).find('a').attr('href');
-    
-     // Check filename
-     if(url == href){
-   
+  // remove # from URL
+  url = url.substring(
+    0,
+    url.indexOf("#") == -1 ? url.length : url.indexOf("#")
+  );
+
+  // remove parameters from URL
+  url = url.substring(
+    0,
+    url.indexOf("?") == -1 ? url.length : url.indexOf("?")
+  );
+
+  // select file name
+  url = url.substr(url.lastIndexOf("/") + 1);
+
+  // If file name not avilable
+  if (url == "") {
+    url = "index.html";
+  }
+
+  // Loop all menu items
+  $(".nav li").each(function () {
+    // select href
+    var href = $(this).find("a").attr("href");
+
+    // Check filename
+    if (url == href) {
       // Add active class
-      $(this).addClass('navActive');
-     }
-    });
-   });
+      $(this).addClass("navActive");
+    }
+  });
+});
 
 //Scroll To Footer on NAV "Contact Me" Click
-   $(document).ready(function() {
-   
-    $('#contactMe').click(function(){
-        $('html, body').animate({scrollTop:$(document).height()}, 'slow');
-        return false;
-    });
+$(document).ready(function () {
+  $("#contactMe").click(function () {
+    $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+    return false;
+  });
 });
+
 //Scroll To Footer on BURGER "Contact Me" Click
-$(document).ready(function() {
-   
-  $('#contactMe2').click(function(){
-      $('html, body').animate({scrollTop:$(document).height()}, 'slow');
-      return false;
+$(document).ready(function () {
+  $("#contactMe2").click(function () {
+    $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+    return false;
   });
 });
 
 //Toggle Burger Menu
-$(document).ready(function(){
-  $(".burger").click(function() {
-    $(this).toggleClass('open');
-    $("ul.burgerContainer li").slideToggle('fast');
-})
+$(document).ready(function () {
+  console.log("READY")
 
+  $(".burger").click(function () {
+    console.log("CLICKING")
+
+    // $("ul.burgerContainer li").toggleClass("open");
+    $("div.burgerContainer").slideToggle("fast");
   });
+});
